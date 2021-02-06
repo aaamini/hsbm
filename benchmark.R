3f409eb8c88e5a9a50bc4394b36e57505c55763c
Arash A. Amini <amini.aa@gmail.com>
2021-02-05 23:47:11 -0800

    code clean-up

--- benchmark.R
+++ benchmark.R
@@ -15,7 +15,6 @@ burnin <- ceiling(niter/2)
 tau = 0.0
 methods = list()
 methods[["HSBM"]] = function(A) {
-  # zh = hsbm_infer(A, beta0=1, gam0=1, ITRmax=ITRmax, Kcap=20, Gcap=20)$zb
   zh = fit_hsbm(A, beta0=0.1, gam0=.5, niter=niter, Kcap=10, Gcap=10, seq_g_update = F, verb = F)$zb
   get_map_labels(zh, burnin = burnin, consecutive = T)$labels
 }
@@ -31,7 +30,7 @@ mtd_names = names(methods)
 res = do.call(rbind, lapply(1:length(methods), function(j) {
   dt = as.numeric(system.time( zh <- methods[[j]](A) )["elapsed"])
   agg_nmi = get_agg_nmi(zb, zh)
-  slice_nmi = get_slice_nmi(zb, zh) # mean(compute_slice_nmi(zb, zh))
+  slice_nmi = get_slice_nmi(zb, zh) 
   data.frame(method_name = mtd_names[j], aggregate_nmi = agg_nmi, 
              slicewise_nmi = slice_nmi, elapsed_time = dt)
 }))
