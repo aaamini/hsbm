6c9c862185efd7656e67b6338b3eaa7d7ddbab1d
Arash A. Amini <amini.aa@gmail.com>
2021-02-07 11:51:39 -0800

    Updated Markov Experiment

--- markov_experim.R
+++ markov_experim.R
@@ -1,10 +1,11 @@
 library(nett)
 library(hsbm)
 
-ncores = max(parallel::detectCores() - 1, 20) # number of cores to use to parallel
-nreps = 25 # number of replications
+ncores = min(parallel::detectCores() - 1, 32) # number of cores to use to parallel
+nreps = 100 # number of replications
 ntrans = 8
 seq_g_update = F
+save_data = T
 n = 200 # number of nodes in each layer
 nlayers = 5 # number of layers
 
@@ -13,14 +14,12 @@ burnin <- ceiling(niter/2)
 
 tau = 0.0
 methods = list()
-methods[["HSBM"]] = function(A, K) {
-  # K is not used
+methods[["HSBM"]] = function(A, K) {# K is not used
   zh = fit_hsbm(A, beta0=0.1, gam0=.5, niter=niter, Kcap=10, Gcap=10, 
                 seq_g_update = seq_g_update, verb = F)$zb
   get_map_labels(zh, burnin = burnin, consecutive = T)$labels
 }
-methods[["DP-SBM"]] =  function(A, K) {
-   # K is not used
+methods[["DP-SBM"]] =  function(A, K) {# K is not used
    zh = fit_mult_dpsbm(A, gam0=.5, niter=niter, Zcap=10, verb = F)$zb
    get_map_labels(zh, burnin = burnin, consecutive = T)$labels
 }
@@ -28,7 +27,6 @@ methods[["SC-sliced"]] = function(A, K) spec_clust_sliced(A, K, tau = tau)
 methods[["SC-avg"]] = function(A, K) spec_clust_avg(A, K, tau = tau)
 
 mtd_names = names(methods)
-
 
 runs = expand.grid(
   mtd_idx = seq_along(methods), 
@@ -56,17 +54,29 @@ total_time = system.time(
 )["elapsed"]
 nett::printf("Total simulation time = %3.2f (s)\n" , total_time)
 
+file_tag = sprintf("markov_exp_n%d_nla%d_nre%d_ntr%d_seq%d", n, nlayers, nreps, ntrans, seq_g_update)
+if (save_data) save.image(paste0(file_tag,".RData"))
+
 library(ggplot2)
 library(dplyr)
 res %>% 
   group_by(method, trans_prob) %>% 
   summarise_all(mean) %>% 
- # mutate(method = as.factor(method)) %>% 
-  ggplot(aes(x = trans_prob, y = aggregate_nmi, color = method)) + 
-  geom_line() +
-  theme_minimal() +
-  xlab("Transition Prob.") +
-  ylab("Aggregate NMI")
+  mutate(method = factor(method, levels = mtd_names)) %>%   # Make the ordered in the order they are defined
+  ggplot(aes(x = trans_prob, y = aggregate_nmi, color = method, linetype = method, shape = method)) + 
+  geom_line(size = 1.25) +
+  # scale_y_continuous( trans = "log10") +
+  geom_point(size = 3) +
+  theme_minimal(base_size = 16) +
+  xlab("Transition Probability") +
+  ylab("Aggregate NMI") + 
+  ggplot2::theme(
+    legend.background = ggplot2::element_blank(),
+    legend.title = ggplot2::element_blank(),
+    legend.position = c(0.8, 0.5),
+    # legend.text = ggplot2::element_text(size=18),
+  ) + 
+  ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 4, keyheight = 1.25))
 
-ggsave(sprintf("markov_exp_n%d_nla%d_nre%d_ntr%d_seq%d.pdf", n, nlayers, nrep, ntrans, seq_g_update))
+ggsave(paste0(file_tag, ".pdf"), width = 7)
   
