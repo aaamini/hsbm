e5f1901d6f06f011367a9c00473cfbf64b5de5bf
Arash A. Amini <amini.aa@gmail.com>
2021-06-19 18:18:30 -0700

    updated markov experiments

--- markov_experim_nlay.R
+++ markov_experim_nlay.R
@@ -1,18 +1,14 @@
 library(nett)
 library(hsbm)
-library(ggplot2)
-library(dplyr)
-library(doParallel)
 
 seed = 1400
 ncores = min(parallel::detectCores() - 1, 32) # number of cores to use to parallel
 nreps = 500 # number of replications
-ntrans = 8
+trans_prob = 0.25
 seq_g_update = F
 save_data = T
 n = 200 # number of nodes in each layer
-nlayers = 5 # number of layers
-random_net = T
+nlay_vec = seq(2,12,2) # number of layers
 
 niter <- 100 # number of Gibbs iterations 
 burnin <- ceiling(niter/2)
@@ -37,12 +33,7 @@ methods[["PisCES-sh"]] = function(A, K) pisces(A, K, shared_kmeans_init = T, ver
 
 mtd_names = names(methods)
 
-runs = expand.grid(
-  mtd_idx = seq_along(methods), 
-  trans_prob = seq(0, 1, length.out = ntrans),
-  rep = 1:nreps  
-)
-
+library(doParallel)
 cl <- parallel::makeForkCluster(ncores)
 doParallel::registerDoParallel(cl)
 doRNG::registerDoRNG(seed)
@@ -53,17 +44,20 @@ gen_rand_eta = function() {
   (eta + t(eta))/2
 }
 
+runs = expand.grid(
+  mtd_idx = seq_along(methods), 
+  nlayers = nlay_vec,
+  rep = 1:nreps  
+)
+
 total_time = system.time(
   #res <- do.call(rbind, parallel::mclapply(1:nrow(runs), function(j) {
   #res <-  do.call(rbind, lapply(1:nrow(runs), function(j) {
   res <- do.call(rbind, foreach::foreach(j = 1:nrow(runs)) %dopar% {
     mi = runs[j, "mtd_idx"]
-    trans_prob = runs[j, "trans_prob"]
-    if (random_net) {
-      out = sample_markov_sbm(n, nlayers, gen_rand_eta(), trans_prob = trans_prob)
-    } else {
-      out = sample_personality_net(n, nlayers, trans_prob = trans_prob) # , seed=1400)   
-    }
+    nlayers = runs[j, "nlayers"]
+    out = sample_personality_net(n, nlayers, trans_prob = trans_prob) # , seed=1400) 
+    # out = sample_markov_sbm(n, nlayers, gen_rand_eta(), trans_prob = trans_prob)
     Ktru = nrow(out$eta)
     A = out$A
     zb = out$zb
@@ -72,58 +66,45 @@ total_time = system.time(
     data.frame(method = mtd_names[mi], 
                aggregate_nmi = get_agg_nmi(zb, zh), 
                slicewise_nmi = get_slice_nmi(zb, zh) , 
-               elapsed_time = dt, trans_prob = trans_prob)
+               elapsed_time = dt, nlayers = nlayers)
   })
   #}))
   #}, mc.cores = ncores, mc.cleaup = T))
 )["elapsed"]
 nett::printf("Total simulation time = %3.2f (s)\n" , total_time)
 
-if (random_net) {
-  file_tag = sprintf("markov_exp_rnd_n%d_nla%d_nre%d_ntr%d_seq%d", n, nlayers, nreps, ntrans, seq_g_update)
-  yrange = c(0, 0.8)
-} else {
-  file_tag = sprintf("markov_exp_n%d_nla%d_nre%d_ntr%d_seq%d", n, nlayers, nreps, ntrans, seq_g_update)  
-  yrange = c(0, 1)
-}
-
+# file_tag = sprintf("markov_exp_rnd_n%d_nla%d_nre%d_ntr%d_seq%d", n, nlayers, nreps, ntrans, seq_g_update)
+file_tag = sprintf("nlay_exp_rnd_n%d_%2.2f_nre%d_nlv%d_seq%d", n, trans_prob, nreps, length(nlay_vec), seq_g_update)
+file_tag = gsub("\\.","p",file_tag)
 if (save_data) save.image(paste0(file_tag,".RData"))
 
-
+library(ggplot2)
+library(dplyr)
 res %>% 
-  group_by(method, trans_prob) %>% 
+  group_by(method, nlayers) %>% 
   summarise_all(mean) %>% 
   mutate(method = factor(method, levels = mtd_names)) %>%   # Make the ordered in the order they are defined
-  ggplot(aes(x = trans_prob, y = aggregate_nmi, color = method, linetype = method, shape = method)) + 
+  ggplot(aes(x = nlayers, y = aggregate_nmi, color = method, linetype = method, shape = method)) + 
   geom_line(size = 1.25) +
   # scale_y_continuous( trans = "log10") +
   geom_point(size = 3) +
   theme_minimal(base_size = 16) +
-  xlab("Transition Probability") +
+  xlab("Number of layers") +
   ylab("Aggregate NMI") + 
-  ylim(yrange) +
-  # ylim(c(0,1)) +
+  scale_x_continuous(breaks = nlay_vec)+
+  ylim(c(0,1)) +
+  # ylim(c(0, .8)) +
   ggplot2::theme(
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
-    legend.position = c(0.8, 0.5),
+    legend.position = c(0.8, 0.6),
     # legend.text = ggplot2::element_text(size=18),
   ) + 
   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 4, keyheight = 1.25))
 
-ggsave(paste0(file_tag, ".pdf"), width = 7)
+ggsave(paste0(file_tag, ".pdf"), width = 6, height = 6)
   
-sum_res = res %>% 
-  filter(trans_prob == unique(res$trans_prob)[5]) %>% 
-  select(-trans_prob) %>% 
-  group_by(method) %>% 
-  summarise(agg_mean = mean(aggregate_nmi), agg_sd = sd(aggregate_nmi), # sd(aggregate_nmi)/sqrt(nreps), 
-            slice_mean = mean(slicewise_nmi), slice_sd = sd(slicewise_nmi), 
-            time_mean = mean(elapsed_time), time_sd = sd(elapsed_time), ) %>% 
-  arrange(desc(agg_mean))
+View(res %>% 
+  group_by(method, nlayers) %>% 
+  summarise_all(sd))
 
-print( knitr::kable(sum_res, digits = 2, format="pipe") )
-
-
-  
- 
